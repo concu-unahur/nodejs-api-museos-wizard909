55cbd02654d4ee09b865eed5712d0e8c2219c445
wizardo909 <cicherofacundo9@gmail.com>
2020-03-10 20:51:43 -0300

    falta

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
 const superagent = require('superagent');
+const fs = require('fs');
 
-function imprimirMuseos(error, respuesta) {
+function escribirMuseos(error, respuesta) {
   if (error) {
     throw new Error('algo se rompió', error);
   }
 
   const cantidad = respuesta.body.count;
   const museos = respuesta.body.results;
+  
 
   console.log(`Se encontraron ${cantidad} museos.`);
-  console.log(`El primer museo se llama ${museos[0].nombre}.`)
+
+  for (i=0; i<cantidad ;i++){
+    console.log(`Museo ${i} escrito.`)
+    fs.writeFile('museos.txt', `${museos[i].nombre}( 
+      ${museos[i].direccion}). Por cualquier consulta comunicarse al 
+      ${museos[i].telefono}. \n`, despuesDeEscribir);
 }
 
-console.log('Antes de llamar a superagent')
+function despuesDeEscribir(error) {
+  if (error) {
+    throw new Error('algo se rompió', error);
+  }
+
+  console.log("Termine de escribir")
+}
 
 superagent
-  .get('https://www.cultura.gob.ar/api/v2.0/museos')
-  .query({ format: 'json' })
-  .end(imprimirMuseos)
-
-console.log('Después de llamar a superagent')
+.get('https://www.cultura.gob.ar/api/v2.0/museos')
+.query({ format: 'json' })
+.end(escribirMuseos)
